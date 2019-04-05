11a3e4a3e1c2ca0e106dfd29efd67aea462c5609
Stella McEwen <mcewen.stella@gmail.com>
2019-04-05 20:12:34 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -13,10 +13,8 @@
 # 'I LOVE YOU TOO PUMPKIN!'
 def speak_to_grandma(greeting)
   if greeting == "I LOVE YOU GRANDMA"
-
-if speak_to_grandma(greeting) == "I LOVE YOU GRANDMA!"
-  puts "I LOVE YOU TOO PUMPKIN!"
-  elsif speak_to_grandma == #{greeting}.upcase
+    puts "I LOVE YOU TOO PUMPKIN"
+  elsif greeting = greeting.upcase
   puts "NO, NOT SINCE 1938!"
 else
   puts "HUH?! SPEAK UP, SONNY!"
