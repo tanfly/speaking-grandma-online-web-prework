74e9f65ef3fb0e49ebf733aa7158531586341fcb
Stella McEwen <mcewen.stella@gmail.com>
2019-04-05 20:21:40 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,9 +1,9 @@
 def speak_to_grandm(string)
   if string != string.upcase
-    puts "HUH?! SPEAK UP, SONNY!"
+    return "HUH?! SPEAK UP, SONNY!"
   elsif string.upcase
-  puts "NO, NOT SINCE 1938!"
+  return "NO, NOT SINCE 1938!"
  elsif string == "I LOVE YOU GRANDMA!"
- puts "I LOVE YOU TOO PUMPKIN!"
+ return "I LOVE YOU TOO PUMPKIN!"
 end
 end
